3a0f1c10a5b03ca0b79a1d5c8d7759569dd166ac
DeliaOros <delia_iletchi@yahoo.com>
2019-01-09 23:20:37 +0200

    Initial message

---
+++ Acronym/Acronym/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace Acronym
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = "Text message, a short electronic message designed for communication between short electronic message mobile phone users. Nowadays, text messages can include media (such as photos, music and videos) and are also called media messages.";
+            string myString = "short electronic message";
+            AcronymBuilder myText = new AcronymBuilder(myString, expression);
+        }
+
+    }
+}
